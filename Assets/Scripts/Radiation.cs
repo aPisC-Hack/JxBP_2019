f5afff53f2f67dacdf987c27f11ae7161a468c29
Bendegúz Cseresnyés <cser.bende@gmail.com>
2019-10-26 12:48:55 +0200

    Created Radiation

---
+++ Assets/Scripts/Radiation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radiation : MonoBehaviour
+{
+    public float Intensity = 1, Depth = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 s = transform.localScale;
+        s.x = Intensity;
+        transform.localScale = s;
+    }
+}
