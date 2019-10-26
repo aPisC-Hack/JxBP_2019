47d40974a1a62bd4b6956bef39cb2be228169bf9
Bendegúz Cseresnyés <cser.bende@gmail.com>
2019-10-26 02:44:09 +0200

    LaserMovement

--- Assets/Scripts/LaserHeadMovement.cs
+++ Assets/Scripts/LaserHeadMovement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LaserHeadMovement : MonoBehaviour
 {
+    public float Rotation;
+    Transform transform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        transform.position = new Vector3(
+            Mathf.Clamp(Mathf.Sin(Rotation) * (transform.parent.localScale.x), -transform.parent.localScale.x/2, transform.parent.localScale.x/2 ),
+            Mathf.Clamp(Mathf.Cos(Rotation) * (transform.parent.localScale.y), -transform.parent.localScale.y/2, transform.parent.localScale.y/2)
+        );
+        float crot = Mathf.Atan2(-transform.position.y, -transform.position.x );
+        transform.rotation = Quaternion.Euler(0, 0, crot * Mathf.Rad2Deg + 90);
     }
 }
