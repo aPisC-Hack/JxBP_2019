853af37195d629422551b898bcbcb32fe4e12833
Bendegúz Cseresnyés <cser.bende@gmail.com>
2019-10-26 15:04:44 +0200

    Frame Sprite

--- Assets/Scripts/GameArea.cs
+++ Assets/Scripts/GameArea.cs
@@ -5,17 +5,23 @@ using UnityEngine;
 public class GameArea : MonoBehaviour
 {
     public float Scale = 10;
+    public float TopPad, BottomPad, LeftPad, RightPad; 
     // Start is called before the first frame update
     void Start()
     {
-        float scale = Mathf.Clamp01( Screen.width*1f / Screen.height);
-        GetComponent<Transform>().localScale = new Vector3(Scale*scale, Scale*scale, 1);
+        
         //GetComponent<Transform>().position = Vector3.up *2f * scale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        float aH = Screen.height - TopPad - BottomPad, aW = Screen.width - LeftPad - RightPad;
+        float x = Scale / Screen.height * 2;
+
+        Debug.Log(aH + " " +  aW + " " + x);
+        transform.localScale = new Vector3(aW*x, aH*x, 1);
+        transform.position = new Vector3((LeftPad - RightPad) * x , (BottomPad-TopPad) * x , 0);
         
     }
 }
