0a0e82ab5ff26edd184078d34ae336cdd444feea
Bendegúz Cseresnyés <cser.bende@gmail.com>
2019-10-26 18:48:21 +0200

    add buttons and images, roatation of the laser is solved

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class ButtonScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+        [Serializable]
+        /// <summary>
+        /// Function definition for a button click event.
+        /// </summary>
+        public class ButtonClickedEvent : UnityEvent {}
+
+        // Event delegates triggered on click.
+        [FormerlySerializedAs("onClick")]
+        [SerializeField]
+        private ButtonClickedEvent m_OnPressed = new ButtonClickedEvent();
+
+        public ButtonClickedEvent onPressed
+        {
+            get { return m_OnPressed; }
+            set { m_OnPressed = value; }
+        }
+    bool pressed = false;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pressed = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pressed) m_OnPressed.Invoke();
+    }
+}
--- Assets/Scripts/LaserHeadMovement.cs
+++ Assets/Scripts/LaserHeadMovement.cs
@@ -8,6 +8,7 @@ public class LaserHeadMovement : MonoBehaviour
     public float Rotation;
     public GameObject Radioation;
     public GameObject RadiationPivot;
+    public bool leftButtonPressed = false;
     private float sqrt2 = Mathf.Sqrt(2);
     // Start is called before the first frame update
     void Start()
@@ -20,12 +21,15 @@ public class LaserHeadMovement : MonoBehaviour
     void Update()
     {
         Vector3 psc = transform.parent.lossyScale;
-
         transform.localPosition = new Vector3(
             Mathf.Clamp(Mathf.Sin(Rotation) * sqrt2 * 1, -1, 1) ,
             Mathf.Clamp(Mathf.Cos(Rotation) * sqrt2 , -1, 1)
         );
         float crot = Mathf.Atan2(-transform.localPosition.y, -transform.localPosition.x );
         transform.rotation = Quaternion.Euler(0, 0, crot * Mathf.Rad2Deg + 90);
+    }
+
+    public void Rotate(float f){
+        Rotation += f;
     }
 }
