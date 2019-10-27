e6a2835708b301b5ced65a87b00e2bc6e4189e1e
Bence Fazekas <ast8ns@INF.ELTE.HU>
2019-10-27 08:44:45 +0100

    good cells

--- Assets/Scripts/Mechanics/Cell.cs
+++ Assets/Scripts/Mechanics/Cell.cs
@@ -20,11 +20,14 @@ public class Cell : MonoBehaviour
     public float RadiationImmunity = 1;
     private CellTypes cellType = CellTypes.Empty;
     public int[] neighbours = new int[6];
-    public CellTypes CellType { 
-        get {return cellType;} 
-        set{
-            if(value == CellTypes.Empty) Destroy(gameObject);
-            if(cellType != value){
+    public CellTypes CellType
+    {
+        get { return cellType; }
+        set
+        {
+            if (value == CellTypes.Empty) Destroy(gameObject);
+            if (cellType != value)
+            {
                 switch (value)
                 {
                     case CellTypes.Good:
@@ -45,32 +48,32 @@ public class Cell : MonoBehaviour
                 }
             }
             cellType = value;
-        } 
+        }
     }
 
     Collider2D coll = null;
     private bool needupdate = false;
     public float maxHp = 1000;
     public float hP = 1000;
-    
+
     private float sqrt2 = Mathf.Sqrt(2);
     public float HP { get => hP; set => hP = value; }
-    
+
 
     public Sprite[] deadSprites;
     public Sprite[] goodSprites;
     public Sprite[] badSprites;
     public Sprite[] SensibleSprites;
 
-    
+
 
 
     //Gauss Bell Distribution
     float GaussBellDistribution(float x, float ex, float d2x)
     {
-        d2x = 1/d2x;
-        return 40*Mathf.Exp(-Mathf.Pow((x - ex), 2) / 2 * d2x) / (Mathf.Sqrt(Mathf.PI*Mathf.Sqrt(d2x)));
-        
+        d2x = 1 / d2x;
+        return 40 * Mathf.Exp(-Mathf.Pow((x - ex), 2) / 2 * d2x) / (Mathf.Sqrt(Mathf.PI * Mathf.Sqrt(d2x)));
+
     }
     float Fixed_GaussBellDistribution(float x, float ex, float d2x)
     {
@@ -81,15 +84,15 @@ public class Cell : MonoBehaviour
 
     float Electron_Damage(float dist, float power)
     {
-        return Fixed_GaussBellDistribution(dist*(float)0.3, 1/3, power/200);
+        return Fixed_GaussBellDistribution(dist * (float)0.3, 1 / 3, power / 200);
     }
     float Proton_Damage(float dist, float power, float maxdist)
     {
-        if (dist>maxdist)
+        if (dist > maxdist)
         {
             return 0;
         }
-        return (dist)*power;
+        return (dist) * power;
     }
 
     float GetDistance(Vector3 head)
@@ -100,14 +103,16 @@ public class Cell : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.tag == "Ray"){
+        if (collision.tag == "Ray")
+        {
             coll = collision;
-            needupdate = true;        
-        }    
+            needupdate = true;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.tag == "Ray"){
+        if (collision.tag == "Ray")
+        {
             coll = null;
             needupdate = false;
         }
@@ -124,25 +129,25 @@ public class Cell : MonoBehaviour
         int height = this.gameObject.transform.parent.GetComponent<MapGenerator>().height;
         int x = Mathf.FloorToInt(id / height);
         int y = id % height;
-        if(x % 2 == 0)
+        if (x % 2 == 0)
         {
-            if (x!=0)
+            if (x != 0)
             {
-                if (y!=0)
+                if (y != 0)
                 {
                     neighbours[0] = (x - 1) * height + y - 1;
                 }
-                if (y!=height)
+                if (y != height)
                 {
                     neighbours[2] = (x - 1) * height + y + 1;
                 }
-               
+
             }
 
-            if (y>0)
+            if (y > 0)
             {
                 neighbours[3] = x * height + y - 1;
-                if (y>-1)
+                if (y > -1)
                 {
                     neighbours[1] = x * height + y - 2;
 
@@ -153,13 +158,13 @@ public class Cell : MonoBehaviour
             {
                 neighbours[5] = x * height + y + 1;
 
-                if (y < height+1)
+                if (y < height + 1)
                 {
                     neighbours[4] = x * height + y + 2;
 
                 }
             }
-            
+
         }
         else
         {
@@ -173,7 +178,7 @@ public class Cell : MonoBehaviour
 
         for (int i = 0; i < 6; i++)
         {
-            if(neighbours[i] < 0)
+            if (neighbours[i] < 0)
             {
                 neighbours[i] = -1;
             }
@@ -214,7 +219,7 @@ public class Cell : MonoBehaviour
 
         for (int i = 0; i < 6; i++)
         {
-            if(neighbours[i] != -1)
+            if (neighbours[i] != -1)
             {
                 List<GameObject> gameObjects = this.gameObject.transform.parent.GetComponent<MapGenerator>().cells;
                 Cell cell = null;
@@ -223,14 +228,14 @@ public class Cell : MonoBehaviour
                 {
                     count++;
                 }
-                if (count<gameObjects.Count)
+                if (count < gameObjects.Count)
                 {
                     cell = gameObjects[count].gameObject.GetComponent<Cell>();
                 }
-                
-                if (cell != null &&( cell.CellType == CellTypes.Cancer && cellType != CellTypes.Dead && Will_Spread(HP, cell.HP, maxHp)))
+
+                if (cell != null && (cell.CellType == CellTypes.Cancer && cellType != CellTypes.Dead && Will_Spread(HP, cell.HP, maxHp)))
                 {
-                    
+
                     this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)cell.CellType] -= 1;
                     CellType = CellTypes.Cancer;
                     this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)cell.CellType] += 1;
@@ -250,11 +255,11 @@ public class Cell : MonoBehaviour
         }
         else if (type == 1)
         {
-            return Electron_Damage(dist, power)/ RadiationImmunity;
+            return Electron_Damage(dist, power) / RadiationImmunity;
         }
         else if (type == 2)
         {
-            return Proton_Damage(dist, power, 4)/ RadiationImmunity;
+            return Proton_Damage(dist, power, 4) / RadiationImmunity;
         }
 
         return 0;
@@ -263,40 +268,40 @@ public class Cell : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (true)
+        if (needupdate)
         {
+            float rotation = coll.gameObject.transform.parent.parent.GetComponent<LaserHeadMovement>().Rotation;
+
+            //proton távolság
+            //float newdistance = GetDistance(new Vector3(Mathf.Sin(rotation), Mathf.Cos(rotation))) * (float)0.5;
+
+            float newdistance = GetDistance(new Vector3(Mathf.Sin(rotation) * sqrt2 * 10, Mathf.Cos(rotation) * sqrt2 * 10)) * (float)0.5;
+
+
+            float distance = GetDistance(coll.gameObject.transform.parent.transform.position) * (float)0.5;
 
 
             //Debug.Log("New: "+newdistance);
             //Debug.Log("Old: " + distance);
-            try{
-            if(this.HP > 0 && needupdate && coll != null && coll.gameObject.transform.parent.parent.GetComponent<LaserHeadMovement>() != null){
-                float intensity = coll.gameObject.transform.parent.GetComponent<Radiation>().Intensity;
-                if(intensity == 0){
-                    needupdate = false;
-                    coll = null;
-                }
-                float rotation = coll.gameObject.transform.parent.parent.GetComponent<LaserHeadMovement>().Rotation;
-
-                //proton távolság
-                //float newdistance = GetDistance(new Vector3(Mathf.Sin(rotation), Mathf.Cos(rotation))) * (float)0.5;
-
-                float newdistance = GetDistance(new Vector3(Mathf.Sin(rotation) * sqrt2 * 10, Mathf.Cos(rotation) * sqrt2 * 10)) * (float)0.5;
-                
-
-                float distance = GetDistance(coll.gameObject.transform.parent.transform.position) * (float)0.5;
-                float dosis = Proton_Damage(distance, intensity, 4);
+            float intensity = coll.gameObject.transform.parent.GetComponent<Radiation>().Intensity;
+            if (intensity == 0)
+            {
+                needupdate = false;
+                coll = null;
+            }
+            if (this.HP > 0)
+            {
+                float dosis = calculateDosis(distance, intensity, (int)coll.gameObject.transform.parent.parent.GetComponent<LaserHeadMovement>().RadiationType);
                 this.HP -= dosis;
                 transform.parent.GetComponent<DosisCalculator>().addDosis(dosis);
-               // this.HP -= Mathf.Abs(GaussBellDistribution(distance, 1 / 3, intensity)) / 4 / RadiationImmunity;
+                // this.HP -= Mathf.Abs(GaussBellDistribution(distance, 1 / 3, intensity)) / 4 / RadiationImmunity;
             }
-            }catch{}
             if (this.HP <= 0 && CellType != CellTypes.Dead)
             {
                 this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)CellType] -= 1;
-                CellType = CellTypes.Dead;                
+                CellType = CellTypes.Dead;
                 this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)CellType] += 1;
-                if (this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)CellTypes.Cancer] <=0)
+                if (this.gameObject.transform.parent.GetComponent<MapGenerator>().sums[(int)CellTypes.Cancer] <= 0)
                 {
                     SaveHandler.DestroySave(MapGenerator.saveid);
                     transform.parent.GetComponent<DosisCalculator>().GameWonDialog();
@@ -314,7 +319,7 @@ public class Cell : MonoBehaviour
             }
         }
         Color c = GetComponent<SpriteRenderer>().color;
-        c.a = hP > 0 ? (hP * 1f / maxHp / 2) +0.5f : 1;
+        c.a = hP > 0 ? (hP * 1f / maxHp / 2) + 0.5f : 1;
         GetComponent<SpriteRenderer>().color = c;
 
     }
